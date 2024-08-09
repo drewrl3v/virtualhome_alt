e3fe46d9b91a15f7a7425e2d426517c71cb2d38b
drewrl3v <drewrl3v@gmail.com>
2024-08-09 13:27:19 -0700

    setting up an alternative virtualhome

--- setup.py
+++ setup.py
@@ -3,42 +3,32 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# Read the requirements.txt file and convert to a list
+with open("./virtualhome/requirements.txt", "r", encoding="utf-8") as f:
+    install_requires = f.read().splitlines()
+
+# This is an alternative repo to the standard virtualhome repo created by Xavier Puig
 setuptools.setup(
     name="virtualhome",
-    version="2.3.0",
-    author="Xavier Puig",
-    author_email="xavierpuig@csail.mit.edu",
+    version="2.3.0_alt",
+    author="Andrew Lizarraga", # Original Author: Xavier Puig
+    author_email="drewrl3v@gmail.com",
     description="Python API to communicate with the VirtualHome environment",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/xavierpuigf/virtualhome",
-    project_urls={
-        "Documentation": "http://virtual-home.org/docs/",
-        "Bug Tracker": "https://github.com/xavierpuigf/virtualhome/issues"
-    },
+    url="https://github.com/drewrl3v/virtualhome_alt",
+    #project_urls={
+    #    "Documentation": "http://virtual-home.org/docs/",
+    #    "Bug Tracker": "https://github.com/xavierpuigf/virtualhome/issues"
+    #},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        'certifi==2022.12.7',
-        'chardet==3.0.4',
-        'idna==2.8',
-        'matplotlib>=3.4.2',
-        'networkx==2.3',
-        'numpy>=1.19.3',
-        'opencv-python==4.5.1.48',
-        'pillow>=8.3.1',
-        'plotly==3.10.0',
-        'requests>=1.21.0',
-        'ipdb==0.13.9',
-        'termcolor==1.1.0',
-        'tqdm==4.31.1',
-        'urllib3>=1.24.3'
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    # Found in the requirements.txt
+    install_requires = install_requires,
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
     python_requires=">=3.10",
 )
-
--- virtualhome/__init__.py
+++ virtualhome/__init__.py
@@ -1,11 +1,36 @@
-import glob
-import sys
-from sys import platform
+#import glob
+#import sys
+#from sys import platform
+#
+## Needs to be fixed!
+#original_path = sys.path[5]
+#new_path = original_path + '/virtualhome/simulation'
+#sys.path.append(new_path)
+#
+#from unity_simulator.comm_unity import UnityCommunication
+#from unity_simulator import utils_viz
+#
 
-# Needs to be fixed!
-original_path = sys.path[5]
-new_path = original_path + '/virtualhome/simulation'
-sys.path.append(new_path)
-
-from unity_simulator.comm_unity import UnityCommunication
-from unity_simulator import utils_viz
+##################################################################
+##################################################################
+##################################################################
+#import os
+#import sys
+#
+## Get the current script's directory
+#current_dir = os.path.dirname(os.path.abspath(__file__))
+#
+## Construct the new path
+#new_path = os.path.join(current_dir, 'virtualhome', 'simulation')
+#
+## Add the new path to sys.path if it's not already there
+#if new_path not in sys.path:
+#    sys.path.append(new_path)
+#
+## Import necessary modules
+##try:
+##    from unity_simulator.comm_unity import UnityCommunication
+##    from unity_simulator import utils_viz
+##except ImportError as e:
+##    raise ImportError(f"Could not import necessary modules: {e}")
+##
